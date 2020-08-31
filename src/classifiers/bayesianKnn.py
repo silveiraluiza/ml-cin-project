30b071d5474496ac406df637a47720ba9f5c05af
marcosd3souza <marcosd3souza@gmail.com>
2020-08-31 16:22:28 -0300

    added train/test and cross validations

--- src/classifiers/bayesianKnn.py
+++ src/classifiers/bayesianKnn.py
@@ -1,24 +1,22 @@
 # @author: marcos de souza
-from sklearn.metrics import pairwise_distances
 import numpy as np
-from helpers import preprocessing as preprocess
+from sklearn.neighbors import NearestNeighbors
+from sklearn.base import BaseEstimator, ClassifierMixin
 
-class BayesianKnnClassifier:
-    idx_neighbor = None
-    y_neighbor = None
-
-    def __init__(self, k):
+class BayesianKnnClassifier(BaseEstimator, ClassifierMixin):
+    def __init__(self, k, log=False):
         self.k = k
+        self.log = log
     
     
-    def density(self, label, y):
-        y_arr = [y[i][0] for i in range(0, len(y))]
+    def density(self, label):
+        y_arr = [self.y[i] for i in range(0, len(self.y))]
         density = len([l for l in y_arr if l == label]) / len(y_arr)
 
         return density
 
     
-    def posteriori(self, xi, y):
+    def posteriori(self, xi):
         # P(wj/xi) = P(xi/wj)*P(wj)
         n_classes = np.unique(xi)
         estimation = 0
@@ -26,7 +24,7 @@ class BayesianKnnClassifier:
 
         for c in n_classes:
             likelihood = xi.count(c) / len(xi)
-            c_density = self.density(c, y)
+            c_density = self.density(c)
             c_estimation = likelihood * c_density
 
             if c_estimation > estimation:
@@ -36,13 +34,32 @@ class BayesianKnnClassifier:
         return estimation, label
 
 
-    def classify(self, X, y):
-        self.idx_neighbor, self.y_neighbor = preprocess.construct_knn_neighborhood(X, y, k=self.k)
+    def fit (self, X, y):
+        # construct the neighborhood
+        self.y = y
+        self.nbh = NearestNeighbors(n_neighbors=self.k, algorithm='auto', metric='euclidean').fit(X)
+    
+    def get_nn_label(self, X):
+        idx_nn = self.nbh.kneighbors(X, return_distance=False)
+
+        nn_labels = []
+        for obj in range(0, len(idx_nn)):
+            nn_labels.append([])
+            for nn in idx_nn[obj]:
+                label = [self.y[i] for i in range(0, len(self.y)) if i == nn][0]
+                nn_labels[obj].append(label)
+        return nn_labels
+
+
+    def predict(self, X):
+        nn_labels = self.get_nn_label(X)
         
         labels = []
-        for xi in self.y_neighbor:
-            estimation, label = self.posteriori(xi, y)
-            print('obj: ', xi, 'estimation:', estimation, 'label: ', label)
+        for xi in nn_labels:
+            estimation, label = self.posteriori(xi)
+
+            if self.log:
+                print('obj: ', xi, 'estimation:', estimation, 'label: ', label)
             labels.append(label)
 
         return labels
--- src/helpers/preprocessing.py
+++ src/helpers/preprocessing.py
@@ -1,10 +1,9 @@
 # @autor marcos de souza
 import numpy as np
 import pandas as pd
-from sklearn.metrics import pairwise_distances
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
-from sklearn.metrics.pairwise import pairwise_distances
+
 
 def normalize_and_split_data(X, y):
     X_scaled = preprocessing.scale(X)
@@ -13,24 +12,6 @@ def normalize_and_split_data(X, y):
 
     return X_train, X_test, y_train, y_test
 
-
-def construct_knn_neighborhood (X, y, k):
-    # compute euclidean distances
-    D = pairwise_distances(X)
-    D **= 2
-    # sort the distance matrix D in ascending order
-    idx = np.argsort(D, axis=1)
-    idx_nn = idx[:, 1:k+1]
-
-    [idx_nn[0][i] for i in range(0, k)]
-
-    y_nn = []
-    for obj in range(0, len(idx_nn)):
-        y_nn.append([])
-        for nn in idx_nn[obj]:
-            label = [y[i][0] for i in range(0, len(y)) if y[i][1] == nn+1][0]
-            y_nn[obj].append(label)
-    return idx_nn, y_nn
 
 
 
